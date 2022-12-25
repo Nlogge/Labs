49f2a4bac71ce56ffb4a498d31a9a53d4ceb67ea
Nlogge <121270427+Nlogge@users.noreply.github.com>
2022-12-25 14:39:56 +0300

    Zad2

---
+++ Zad2/Zad2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+    double x = double.MaxValue;
+    double y = 0.0000000000000000000000001;
+    Swap(ref x, ref y);
+    Console.WriteLine($"{x} {y}");
+    Console.ReadKey();
+
+
+    static void Swap<T>(ref T x, ref T y)
+    {
+        T t = y;
+        y = x;
+        x = t;
+    }
+
