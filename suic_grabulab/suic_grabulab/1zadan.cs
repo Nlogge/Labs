881b1e7563249aa93a2976eb07634c7ea9a56878
Nlogge <121270427+Nlogge@users.noreply.github.com>
2022-12-25 14:38:49 +0300

    Labs1 zad1

---
+++ suic_grabulab/suic_grabulab/1zadan.cs
@@ -0,0 +1,17 @@
+﻿// 1 задание
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+Console.WriteLine("Введите число");
+try
+{
+    int Count = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Count);
+}
+catch {
+    Console.WriteLine("Введено не число");
+}
+
+
+
+
