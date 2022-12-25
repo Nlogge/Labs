a7d8546d89b415259204cb42bdf4785d2e99ce07
Nlogge <121270427+Nlogge@users.noreply.github.com>
2022-12-25 14:40:41 +0300

    Zad5

---
+++ Zad5/Zad5/Program.cs
@@ -0,0 +1,54 @@
+﻿
+Console.WriteLine("Введите число соответствующие месяцу");
+try
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    switch (x)
+    {
+        case 1:
+            Console.WriteLine("Январь");
+            break;
+        case 2:
+            Console.WriteLine("Февраль");
+            break;
+        case 3:
+            Console.WriteLine("Март");
+            break;
+        case 4:
+            Console.WriteLine("Апрель");
+            break;
+        case 5:
+            Console.WriteLine("Май");
+            break;
+        case 6:
+            Console.WriteLine("Июнь");
+            break;
+        case 7:
+            Console.WriteLine("Июль");
+            break;
+        case 8:
+            Console.WriteLine("Август");
+            break;
+        case 9:
+            Console.WriteLine("Сентябрь");
+            break;
+        case 10:
+            Console.WriteLine("Октябрь");
+            break;
+        case 11:
+            Console.WriteLine("Ноябрь");
+            break;
+        case 12:
+            Console.WriteLine("Декабрь");
+            break;
+        default:
+            Console.WriteLine("Введенно число не соответствующие месяцу");
+            break;
+
+    }
+
+}
+catch {
+    Console.WriteLine("Введено не число");
+
+}
